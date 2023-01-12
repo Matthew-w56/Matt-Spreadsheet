76f2c3d7bc1debbe00ec06c62b9c500249ef80eb
Matthew Williams <mbwilliams785@gmail.com>
2023-01-12 15:08:23 -0700

    Filled out comments and added Evaluate method

---
+++ FormulaEvaluator/Evaluator.cs
@@ -0,0 +1,28 @@
+﻿namespace FormulaEvaluator
+{
+    /// <summary>
+    /// Author: Matthew Williams
+    /// Date:   January 12th, 2023
+    /// Course: CS 3500
+    /// 
+    /// I, Matthew Williams, certify that I wrote this code myself and did not steal ideas or
+    /// copy the work of another.
+    /// 
+    /// This Library provides the Evlauator that takes in a string representation of a formula
+    /// and returns the answer to that formula.
+    /// </summary>
+    public class Evaluator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="expression">The mathematical expression to be evaluated</param>
+        /// <param name="variableEvaluator">A Lookup containing the variables used in the formula</param>
+        /// <returns></returns>
+        public static int Evaluate( String expression, Lookup variableEvaluator )
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+    }
+}
